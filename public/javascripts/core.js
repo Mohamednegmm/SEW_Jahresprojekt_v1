bad050bec7d64ee6153614768f1a6a843de6dd1b
Mohamednegmm <m.negm97@htl-ottakring.ac.at>
2017-05-15 15:02:53 +0200

    Initial commit

---
+++ public/javascripts/core.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+
+    console.log("ok");
+
+    $.ajax({
+        method: "GET",
+        url: "/ajaxteam",
+        success: function (data) {
+            var element = JSON.parse(data);
+            for (var block of element.Teamdaten) {
+                $('#team').append(
+
+                    "<div class='col-sm-3 text-center'>" +
+                    "<div class='team-member'>" +
+                    "<img src='" + block.img + "' class='img-circle' alt=''>" +
+                    "<br>" + "<br>" +
+                    "<h4>" + block.name + "</h4>" +
+                    "<p>" + block.role + "</p>" +
+                    "</div>" +
+                    "</div>"
+
+                );
+            }
+
+        }
+    });
+});
